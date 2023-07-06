119b8fe5ccf80010af6cf2a23b607ab911707904
VijayarajB <vijayaraj.balan@ceiyone.com>
2023-07-06 09:12:16 +0530

    Footer changes

--- spotandplay/src/Footer.js
+++ spotandplay/src/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 import GooglePlay from './logos/GooglePlay.png';
 import AppStore from './logos/AppStore.png';
 import SPLogo from './logos/splogo.png';
-import Instagram from './logos/instagram.png';
-import Linkedin from './logos/linkedin.png';
-import Facebook from './logos/facebook.png';
-import Twitter from './logos/twitter.png'
+import Instagram from './logos/instagram_color.png';
+import Linkedin from './logos/linkedin_color.png';
+import Facebook from './logos/fb_color.png';
+import Twitter from './logos/twitter_color.png'
 import './Footer.css';
 
 const Footer = () => {
@@ -26,15 +26,21 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-column">
+        <div class="footer-widget-heading">
+              <h3>Useful Links</h3>
+            </div>
           <ul className="footer-links">
-            <li>About Us</li>
-            <li>Contact</li>
-            <li>Careers</li>
-            <li>Partner with Us</li>
+            <li>Home</li>     
+            <li>About us</li>
+            <li>Our Services</li>
+            <li>Contact us</li>
           </ul>
         </div>
         <div className="footer-column">
-          <div className="footer-social-icons">
+        <div class="footer-widget-followus">
+              <h3>Follow us</h3>
+            </div>      
+          <div className="footer-social-icons">      
             <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
               <img src={Instagram} alt="Instagram" className="footer-social-icon" />
             </a>
@@ -50,16 +56,18 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="footer-row">
-        <div className="footer-links-row">
-          <a href="/faqs">FAQs</a>
-          <a href="/privacy-policy">Privacy Policy</a>
-          <a href="/terms-of-service">Terms of Service</a>
-          <a href="/cancellation-policy">Cancellation Policy</a>
-        </div>
+      <div className='copyright-text'>
+      <div className="footer-row2">
         <p className="footer-text">© 2023 Spot &amp; Play. All Rights Reserved.</p>
+        <div className="footer-links-row">
+          <a href="/home">Home</a>
+          <a href="/terms">Terms</a>
+          <a href="/privacy">Privacy</a>
+          <a href="/policy">Policy</a>
+          <a href="/contact">Contact</a>
+        </div>
       </div>
-      
+      </div>
     </footer>
   );
 };
